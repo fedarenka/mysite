3c9f6ce3d100adc7444c7ece7cc2eaeaece5aa68
Svetlana <s.fedarenka@mail.ru>
2022-07-20 22:08:18 +0300

    django

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import *
-
-admin.site.register(Book)
 admin.site.register(Review)
+admin.site.register(Shop)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'author')
+    exclude = ('pages',)
+admin.site.register(Book, BookAdmin)
